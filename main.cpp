cd358ae7f10e5e88991086e6e64fe93b93d2bd6c
divyarao54 <110017858+divyarao54@users.noreply.github.com>
2025-03-14 20:34:53 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Jenkins!" << std::endl;
+    std::cout << "This is a test C++ program." << std::endl;
+    std::cout << "Pipeline execution successful!" << std::endl;
+    return 0;
+}
